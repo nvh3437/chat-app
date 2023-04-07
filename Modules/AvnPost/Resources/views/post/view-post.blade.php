71b702f780ac3fad1801d2dc19560c581384aee5
Huu Chien <huuchienfg@gmail.com>
2023-04-07 17:46:54 +0700

    update post va user
    update post va user

--- Modules/AvnPost/Resources/views/post/view-post.blade.php
+++ Modules/AvnPost/Resources/views/post/view-post.blade.php
@@ -1,3 +1,6 @@
+@php
+    $user = App\Http\Controllers\Controller::getUser(); 
+@endphp
 @extends('layouts.guest')
 @section('title')
     Xem bài viết
@@ -23,6 +26,107 @@
                         {!! $post->description !!}
                     </div>
                 </div>
+            </div>
+            <div class="row">
+                <h4 class="header-title">Bình luận</h4>
+                @if($user == '')
+                    <div class="card">
+                        <div class="card-body">
+                            <a href="{{ route('login') }}" class="text-danger">Vui lòng đăng nhập để bình luận</a>
+                        </div>
+                    </div>
+                @else
+                    <div class="card">
+                        <div class="card-body pb-1">
+                            <form action="{{ route('store-post-comment') }}" method="POST" enctype="multipart/form-data" class="comment-area-box">
+                            @csrf
+                                <input type="hidden" name="post_id" value="{{$post->id}}">
+                                <textarea rows="4" class="form-control border-0 resize-none" placeholder="Nhập bình luận...." name="comment"></textarea>
+                                <div class="p-2 bg-light d-flex justify-content-end align-items-center">
+                                    <button type="submit" class="btn btn-sm btn-success"><i class='uil uil-message me-1'></i>Gửi</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    @foreach($comments as $item)
+                        <div class="card">
+                            <div class="card-body pb-1">
+                                <div class="d-flex">
+                                    <img class="me-2 rounded" src="{{ asset('/resources/assets/images/logo.png') }}" height="32">
+                                    <div class="w-100">
+                                        @if($user->id == $item->user_id)
+                                            <div class="dropdown float-end text-muted">
+                                                <a href="#" class="dropdown-toggle arrow-none card-drop"
+                                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                                    <i class="mdi mdi-dots-horizontal"></i>
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-end">
+                                                    <a href="javascript:void(0);" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#edit-{{ $item->id }}">Sửa</a>
+                                                    <a href="javascript:void(0);" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#delete-{{ $item->id }}">Xóa</a>
+                                                </div>
+                                            </div>
+                                            <!--- Modal Edit -->
+                                            <div class="modal fade" id="edit-{{ $item->id }}" tabindex="-1" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title text-dark">Sửa bình luận</h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <form action="{{ route('update-post-comment', $item->id) }}" method="POST">
+                                                            @csrf
+                                                            @method('PUT')
+                                                            <input type="hidden" name="post_id" value="{{$post->id}}">
+                                                            <div class="modal-body text-dark">
+                                                                <textarea class="form-control" name="comment" rows="5">{!! $item->comment !!}</textarea>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Hủy</button>
+                                                                <button type="submit" class="btn btn-success">Sửa</button>  
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!----Modal Delete----->
+                                            <div class="modal fade" id="delete-{{ $item->id }}" tabindex="-1"
+                                                aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title text-dark">Xác nhận</h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                                aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body text-dark">
+                                                            <p>Bạn có muốn xóa không?</p>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-light" data-bs-dismiss="modal">Hủy
+                                                            </button>
+                                                            <form action="{{ route('delete-post-comment', [$item->id]) }}"
+                                                                method="POST">
+                                                                @csrf
+                                                                @method('delete')
+                                                                <button type="submit" class="btn btn-primary">Xóa</button>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @endif
+                                        <h5 class="m-0">{{$item->post_comment->name}}</h5>
+                                        <p class="text-muted"><small><td>{{ date('d/m/Y', strtotime($item->updated_at)) }}</td></small></p>
+                                    </div>
+                                </div>
+                                <hr class="m-0" />
+                                <div class="font-16 text-start text-dark my-2">
+                                    {!! $item->comment !!}
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach 
+                @endif
             </div>
         </div>
     </section>
