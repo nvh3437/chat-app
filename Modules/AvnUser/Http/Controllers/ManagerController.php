e2698ad31dc0487c977a0f76fdbe7e574d744cb9
Huu Chien <huuchienfg@gmail.com>
2023-04-07 08:39:46 +0700

    update manager
    update manager

---
+++ Modules/AvnUser/Http/Controllers/ManagerController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Modules\AvnUser\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
+
+class ManagerController extends Controller
+{
+    public function managerProfile()
+    {
+        $user = Auth::user();
+        return view('avnuser::manager.manager-profile', compact('user'));
+    }
+
+    public function updateManagerProfile(Request $request)
+    {
+        try {
+            // Lưu bảng user 
+            $user = Auth::user();
+            $user->name = $request->name;
+            if ($user->email != $request->email && $request->email) {
+                $user_change_mail = User::where('email', $request->email)->first();
+                if ($user_change_mail) {
+                    return back()->with('Failed', 'Email đã tồn tại');
+                }
+                $user->email = $request->email;
+            }
+            if ($request->password != null && strlen($request->password) > 0) {
+                $user->password = Hash::make($request->password);
+            }
+            $user->save();
+            return back()->with('Success', 'Cập nhật thành công');
+        } catch (Exception $e) {
+            return back()->with('Failed', 'Cập nhật thất bại');
+        }
+    }
+
+}
--- Modules/AvnUser/Routes/web.php
+++ Modules/AvnUser/Routes/web.php
@@ -30,6 +30,10 @@ Route::prefix('user')->group(function() {
     Route::post('/store-customer', 'CustomerManagerController@storeCustomer')->middleware(['auth', 'permission'])->name('store-customer');
     Route::put('/update-customer/{id}', 'CustomerManagerController@updateCustomer')->middleware(['auth', 'permission'])->name('update-customer');
     Route::delete('/delete-customer/{id}', 'CustomerManagerController@deleteCustomer')->middleware(['auth', 'permission'])->name('delete-customer');
+
+    // Quản lý
+    Route::get('/manager-profile', 'ManagerController@managerProfile')->middleware(['auth'])->name('manager-profile');
+    Route::put('/update-manager-profile', 'ManagerController@updateManagerProfile')->middleware(['auth'])->name('update-manager-profile');
 });
 
 //-------------------------- Khách hàng tự đăng ký, xem thông tin bản thân,... ---------------------//
