ebe53ab92e6aad6dea9f84d9a0950e442711e56f
Huu Chien <huuchienfg@gmail.com>
2023-04-11 16:01:48 +0700

    update new feed
    update new feed

--- Modules/AvnSetting/Resources/views/navbar/navbar.blade.php
+++ Modules/AvnSetting/Resources/views/navbar/navbar.blade.php
@@ -59,6 +59,7 @@
 					                <th>STT</th>
 					                <th>Tên</th>
 					                <th>Đường dẫn</th>
+					                <th>Thứ tự</th>
 					                <th>Sở thuộc</th>
 					                <th>Chọn</th>
 					            </tr>
@@ -72,6 +73,7 @@
 					                    <td>{{ ++$i }}</td>
 					                    <td>{{ $item->name }}</td>
 					                    <td>{{ $item->link }}</td>
+					                    <td>{{ $item->order }}</td>
 					                    <td>
                                             @if($item->parent_id == '0')
                                                 Không có
