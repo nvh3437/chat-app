93338484e908e11902dfee1bcbc8d08b87b19973
gmjonathan281 <102947071+gmjonathan281@users.noreply.github.com>
2023-04-26 11:22:02 +0700

    Update SendMessageUser.php

--- app/Events/SendMessageUser.php
+++ app/Events/SendMessageUser.php
@@ -7,21 +7,20 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class SendMessageUser implements ShouldBroadcastNow
+class SendMessageUser implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $message;
-    public $user_receives;
+    public $user_receive;
     public $user_send;
     public $load_room;
     public $is_system;
-    public function __construct($user_receives, $user_send = null, $message, $load_room = false, $is_system = false)
+    public function __construct($user_receive, $user_send = null, $message, $load_room = false, $is_system = false)
     {
-        $this->user_receives = $user_receives;
+        $this->user_receive = $user_receive;
         $this->user_send = $user_send;
         $this->message = $message;
         $this->load_room = $load_room;
@@ -29,17 +28,8 @@ class SendMessageUser implements ShouldBroadcastNow
     }
     public function broadcastOn()
     {
-        $chanels = [];
-        foreach ($this->user_receives as $key => $user_receive) {
-            $chanels[] = new PrivateChannel('chat.user.' . $user_receive->id); //private
-        }
-        return $chanels;
-        // return new PrivateChannel('chat.user.' . $this->user_receives->id); //private
+        return new PrivateChannel('chat.user.' . $this->user_receive->id); //private
         // return new Channel('chat'); //public
-    }
-    public function broadcastQueue(): string
-    {
-        return 'default';
     }
 
     public function broadcastAs()
