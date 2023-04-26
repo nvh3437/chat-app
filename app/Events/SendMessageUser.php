2d52aefec44f4e751abc4d63b6ab643f59f52f68
gmjonathan281 <102947071+gmjonathan281@users.noreply.github.com>
2023-04-26 10:34:23 +0700

    update

--- app/Events/SendMessageUser.php
+++ app/Events/SendMessageUser.php
@@ -7,20 +7,21 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class SendMessageUser implements ShouldBroadcast
+class SendMessageUser implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $message;
-    public $user_receive;
+    public $user_receives;
     public $user_send;
     public $load_room;
     public $is_system;
-    public function __construct($user_receive, $user_send = null, $message, $load_room = false, $is_system = false)
+    public function __construct($user_receives, $user_send = null, $message, $load_room = false, $is_system = false)
     {
-        $this->user_receive = $user_receive;
+        $this->user_receives = $user_receives;
         $this->user_send = $user_send;
         $this->message = $message;
         $this->load_room = $load_room;
@@ -28,8 +29,17 @@ class SendMessageUser implements ShouldBroadcast
     }
     public function broadcastOn()
     {
-        return new PrivateChannel('chat.user.' . $this->user_receive->id); //private
+        $chanels = [];
+        foreach ($this->user_receives as $key => $user_receive) {
+            $chanels[] = new PrivateChannel('chat.user.' . $user_receive->id); //private
+        }
+        return $chanels;
+        // return new PrivateChannel('chat.user.' . $this->user_receives->id); //private
         // return new Channel('chat'); //public
+    }
+    public function broadcastQueue(): string
+    {
+        return 'default';
     }
 
     public function broadcastAs()
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,12 +2,14 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Env;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use App\Http\Controllers\Helper;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -30,16 +32,20 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
         //
         VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            $company = Helper::getCompanyName();
             return (new MailMessage)
-                ->subject('Japan - Xác nhận địa chỉ email')
-                ->line('Nhấn vào nút bên dưới để xác minh địa chỉ email của bạn trên hệ thống Japan.')
+                ->from(Env::get('MAIL_USERNAME'), $company)
+                ->subject($company . ' - Xác nhận địa chỉ email')
+                ->line('Nhấn vào nút bên dưới để xác minh địa chỉ email của bạn trên hệ thống ' . $company . ' của chúng tôi.')
                 ->action('Xác nhận email', $url);
         });
         ResetPassword::toMailUsing(function (object $notifiable, string $token) {
+            $company = Helper::getCompanyName();
             return (new MailMessage)
-                ->subject('Japan - Đặt lại mật khẩu')
-                ->line('Nhấn vào nút bên dưới để đặt lại mật khẩu của bạn trên hệ thống Japan.')
-                ->action('Đặt lại mật khẩu', route('password.reset', ['token' =>$token]));
+                ->from(Env::get('MAIL_USERNAME'), $company)
+                ->subject($company . ' - Đặt lại mật khẩu')
+                ->line('Nhấn vào nút bên dưới để đặt lại mật khẩu của bạn trên hệ thống ' . $company . ' của chúng tôi.')
+                ->action('Đặt lại mật khẩu', route('password.reset', ['token' => $token]));
         });
     }
 }
