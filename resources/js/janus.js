1efe70d10812de9f04b9fd99b35116c76c2fc6d7
gmjonathan281 <102947071+gmjonathan281@users.noreply.github.com>
2023-05-13 17:36:32 +0700

    update

--- resources/js/janus.js
+++ resources/js/janus.js
@@ -26,7 +26,7 @@
 
 // List of sessions
 Janus.sessions = {};
-const server = "http://janus.avntech.vn/janus";
+const server = "https://janus.avntech.vn/janus";
 const iceServers = [{urls: "stun:stun.l.google.com:19302"}]
 Janus.isExtensionEnabled = function() {
 	if(navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia) {
