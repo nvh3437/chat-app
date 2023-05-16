4b5143ce3dd84970ec2c6e01e7bac5bc23105e8d
gmjonathan281 <102947071+gmjonathan281@users.noreply.github.com>
2023-05-16 18:03:16 +0700

    Update index.blade.php

--- Modules/AvnChat/Resources/views/index.blade.php
+++ Modules/AvnChat/Resources/views/index.blade.php
@@ -559,8 +559,8 @@
                     })
                     .listen('.newCall', (e) => {
                         if (!is_calling && !is_incall && !is_ringing) {
-                            $('.microphone').removeAttr('disabled')
-                            $('.stop-call').removeAttr('disabled')
+                            $('.microphone').addClass('disabled')
+                            $('.stop-call').addClass('disabled')
                             is_ringing = true
                             window.incoming_call_modal.hide()
                             call_id = e.call_id
