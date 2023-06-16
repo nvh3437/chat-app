353559cff8e84bf63887181b97da16cc228895d1
gmjonathan281 <102947071+gmjonathan281@users.noreply.github.com>
2023-06-16 17:04:58 +0700

    Update UpdateProfileRequest.php

--- Modules/AvnUser/Http/Requests/UpdateProfileRequest.php
+++ Modules/AvnUser/Http/Requests/UpdateProfileRequest.php
@@ -11,7 +11,7 @@ class UpdateProfileRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'email' => 'unique:users,email,' . $this->user,
+            'email' => 'unique:users,email,' . $this->email,
         ];
     }
 
