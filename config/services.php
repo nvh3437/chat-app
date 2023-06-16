9d80ab0a0f3d3eebe4ca9252f6e912b2069df164
gmjonathan281 <102947071+gmjonathan281@users.noreply.github.com>
2023-06-16 16:26:32 +0700

    Update services.php

--- config/services.php
+++ config/services.php
@@ -31,13 +31,13 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'facebook' => [
-        'client_id' => '694930635551376',
-        'client_secret' => '365c8ce2faac36a12b9af1b518477608',
+        'client_id' => '844986103261731',
+        'client_secret' => '7d34b9eb18219d6af2b51b50833abf94',
         'redirect' => 'https://hikari-hr.com/login/facebook/callback',
     ],
     'google' => [
-        'client_id' => '332630337654-343bn4hb8e1mcp69h2hh1jfcu8tb16gb.apps.googleusercontent.com',
-        'client_secret' => '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',
+        'client_id' => '584156284032-1r0rejgk9ispiqku5f3erm67s3foorqb.apps.googleusercontent.com',
+        'client_secret' => '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',
         'redirect' => 'https://hikari-hr.com/login/google/callback',
     ],
 ];
